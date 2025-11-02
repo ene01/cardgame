a10624e2dc085bfcc21caa0a3a0dd45b97f7877d
ene01 <masqufeik@gmail.com>
2025-11-02 06:43:14 -0300

    Modify acording to new funtions and structs

--- src/player.zig
+++ src/player.zig
@@ -3,62 +3,46 @@ const card = @import("card.zig");
 const cardmatrix = @import("cardmatrix.zig");
 const deck = @import("deck.zig");
 
-/// Initializes and returns a player's attributes with an empty hand and default rows.
-pub fn init(alloc: std.mem.Allocator) PAttributes {
-    return PAttributes{ .hand = deck.init(alloc), .discard_deck = deck.init(alloc), .rows = cardmatrix.init(), .is_playing = true };
-}
-
 /// Player attributes: hand, rows, and status.
-pub const PAttributes = struct {
-    hand: deck.Cards,
-    discard_deck: deck.Cards,
-    rows: cardmatrix.Matrix,
+pub const Attributes = struct {
+    hand: deck.PlayingCards,
+    discard_deck: deck.PlayingCards,
+    matrix: cardmatrix.CardMatrix,
     is_playing: bool,
 
+    /// Initializes and returns a player's attributes with an empty hand.
+    pub fn init(alloc: std.mem.Allocator, columnAmount: usize) !Attributes {
+        return Attributes{ .hand = deck.PlayingCards.init(alloc), .discard_deck = deck.PlayingCards.init(alloc), .matrix = try cardmatrix.CardMatrix.init(alloc, columnAmount), .is_playing = true };
+    }
+
     /// Releases memory for dynamic arrays (hand).
-    pub fn deinit(self: *PAttributes) void {
+    pub fn deinit(self: *Attributes) void {
         self.hand.deinit();
+        self.matrix.deinit();
     }
 
     /// Adds a card to the player's hand.
-    pub fn addToHand(self: *PAttributes, newCard: card.CAttributes) !void {
+    pub fn addToHand(self: *Attributes, newCard: card.Identity) !void {
         try self.hand.addCard(newCard);
     }
 
     /// Adds a card to the specified row.
-    pub fn addToRow(self: *PAttributes, newCard: card.CAttributes, row: usize) !void {
-        try self.rows.addCard(@intCast(row), newCard);
+    pub fn addToRow(self: *Attributes, newCard: card.Identity, row: usize) !void {
+        try self.matrix.addCard(@intCast(row), newCard);
     }
 
     /// Removes a single instance of a card from the player's hand.
-    pub fn removeFromHand(self: *PAttributes, cardToRemove: card.CAttributes) !void {
-        for (self.hand.cards.items, 0..) |currentCard, index| {
-            if (currentCard.rank == cardToRemove.rank and currentCard.suit == cardToRemove.suit) {
-                _ = self.hand.cards.orderedRemove(index);
-                break;
-            }
-        }
+    pub fn removeCardFromHand(self: *Attributes, cardToRemove: card.Identity) ?usize {
+        return self.hand.removeCardByID(cardToRemove);
     }
 
     /// Removes all instances of a card from the player's hand.
-    pub fn removeCards(self: *PAttributes, cardToRemove: card.CAttributes) !void {
-        var found = true;
-        while (found) {
-            found = false;
-            for (self.hand.cards.items, 0..) |currentCard, index| {
-                if (currentCard.rank == cardToRemove.rank and currentCard.suit == cardToRemove.suit) {
-                    _ = self.hand.cards.orderedRemove(index);
-                    found = true;
-                    break;
-                }
-            }
-        }
+    pub fn removeCardsFromHand(self: *Attributes, cardToRemove: card.Identity) ?usize {
+        return self.hand.removeCardByID(cardToRemove);
     }
 
-    /// Removes and returns the last card from the specified row.
-    pub fn removeFromRow(self: *PAttributes, row: usize) !card.CAttributes {
-        const removed_card = try self.rows.checkMatrix(@intCast(row), self.rows.row_last_index[@intCast(row)]);
-        try self.rows.removeCard(@intCast(row));
-        return removed_card;
+    /// Removes the last card from the specified row.
+    pub fn removeCardFromColumn(self: *Attributes, column: usize) ?card.Identity {
+        return self.matrix.removeCard(column);
     }
 };
