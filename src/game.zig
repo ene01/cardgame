04e3c26ec2ea62eecc53e34d5da648c583b42e85
ene01 <masqufeik@gmail.com>
2025-11-02 06:44:29 -0300

    Modify acording to new funtions and structs

--- src/game.zig
+++ src/game.zig
@@ -5,27 +5,31 @@ const rank = @import("rank.zig");
 const suit = @import("suit.zig");
 const card = @import("card.zig");
 
-/// Initiation of Cardgame.
-pub fn init(alloc: std.mem.Allocator) Cardgame {
-    return Cardgame{ .deck = deck.init(alloc), .player = player.init(alloc) };
-}
-
 // TODO: Needs the game implementation itself...
 pub const Cardgame = struct {
-    deck: deck.Cards,
-    player: player.PAttributes,
+    deck: deck.PlayingCards,
+    player: player.Attributes,
+
+    /// Initiation of Cardgame.
+    pub fn init(alloc: std.mem.Allocator, defaultColumnAmount: usize) !Cardgame {
+        return Cardgame{ .deck = deck.PlayingCards.init(alloc), .player = try player.Attributes.init(alloc, defaultColumnAmount) };
+    }
 
     pub fn giveCardsToPlayer(self: *Cardgame) !void {
-        for (self.player.hand.items.len..5) |_| {
-            const picked_card = try self.deck.randomLookUp();
+        for (self.player.hand.cards.items.len..5) |_| {
+            const picked_card = self.deck.randomLookUp();
 
-            try self.player.addToHand(picked_card);
-            try self.deck.removeSingleCard(picked_card);
+            if (picked_card) |valid_card| {
+                try self.player.addToHand(valid_card);
+                _ = self.deck.removeCardByID(valid_card);
+            } else {
+                return;
+            }
         }
     }
 
     /// Deinit allocator data.
-    pub fn deinit(self: *Cardgame) !void {
+    pub fn deinit(self: *Cardgame) void {
         self.deck.deinit();
         self.player.deinit();
     }
@@ -35,7 +39,7 @@ pub const Cardgame = struct {
         for (0..2) |_| { // Repeat 2 times
             for (1..(@intFromEnum(suit.Group.Diamond)) + 1) |suitInt| {
                 for (2..(@intFromEnum(rank.Hierarchy.Ace)) + 1) |rankInt| {
-                    try self.deck.addCard(card.CAttributes{ .rank = @enumFromInt(rankInt), .suit = @enumFromInt(suitInt) });
+                    try self.deck.addCard(card.Identity{ .rank = @enumFromInt(rankInt), .suit = @enumFromInt(suitInt) });
                 }
             }
         }
