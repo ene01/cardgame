2439cfc39d0ae0ef4b99c01b0d2d89af82969f45
ene01 <masqufeik@gmail.com>
2025-11-02 06:41:13 -0300

    Naming improvements

--- src/card.zig
+++ src/card.zig
@@ -3,17 +3,21 @@ const rank = @import("rank.zig");
 const suit = @import("suit.zig");
 
 /// Card attributes: Rank and Suit.
-pub const CAttributes = struct {
+pub const Identity = struct {
     rank: rank.Hierarchy,
     suit: suit.Group,
 
+    pub fn isCardEqual(cardOne: Identity, cardTwo: Identity) bool {
+        return if (cardOne.rank == cardTwo.rank and cardOne.suit == cardTwo.suit) true else false;
+    }
+
     /// Sets the card's rank.
-    pub fn changeRank(self: *CAttributes, newRank: rank.Hierarchy) void {
+    pub fn changeRank(self: *Identity, newRank: rank.Hierarchy) void {
         self.rank = newRank;
     }
 
     /// Sets the card's suit.
-    pub fn changeSuit(self: *CAttributes, newSuit: suit.Group) void {
+    pub fn changeSuit(self: *Identity, newSuit: suit.Group) void {
         self.suit = newSuit;
     }
 };
