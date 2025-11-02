521024bb1f0685a1b2841b33999fa983266c8b08
ene01 <masqufeik@gmail.com>
2025-11-02 06:38:44 -0300

    Overall code improvement, add missing functions

--- src/deck.zig
+++ src/deck.zig
@@ -3,74 +3,137 @@ const card = @import("card.zig");
 const rank = @import("rank.zig");
 const suit = @import("suit.zig");
 
-/// Create an empty deck with the given allocator.
-pub fn init(allocator: std.mem.Allocator) Cards {
-    return Cards{ .cards = std.ArrayList(card.CAttributes).init(allocator) };
-}
-
 /// A dynamic deck of cards.
-pub const Cards = struct {
-    cards: std.ArrayList(card.CAttributes),
+pub const PlayingCards = struct {
+    cards: std.ArrayList(card.Identity),
+
+    /// Create an empty deck with the given allocator.
+    pub fn init(allocator: std.mem.Allocator) PlayingCards {
+        return PlayingCards{ .cards = std.ArrayList(card.Identity).init(allocator) };
+    }
 
     /// Releases memory used by the deck.
-    pub fn deinit(self: *Cards) void {
+    pub fn deinit(self: *PlayingCards) void {
         self.cards.deinit();
     }
 
+    /// Clears all cards on the deck.
+    pub fn clear(self: *PlayingCards) void {
+        self.cards.clearRetainingCapacity();
+    }
+
+    pub fn len(self: *PlayingCards) usize {
+        return self.cards.items.len;
+    }
+
+    pub fn isEmpty(self: *PlayingCards) bool {
+        return self.cards.items.items.len == 0;
+    }
+
     /// Adds a card to the deck.
-    pub fn addCard(self: *Cards, newCard: card.CAttributes) !void {
+    pub fn addCard(self: *PlayingCards, newCard: card.Identity) !void {
         try self.cards.append(newCard);
     }
 
-    /// Removes a single instance of the given card from the deck.
-    pub fn removeSingleCard(self: *Cards, cardToRemove: card.CAttributes) !void {
-        for (self.cards.items, 0..) |currentCard, index| {
-            if (currentCard.rank == cardToRemove.rank and currentCard.suit == cardToRemove.suit) {
-                _ = self.cards.orderedRemove(index);
-                return;
-            }
+    /// Removes a single instance of the given card ID.
+    pub fn removeCardByID(self: *PlayingCards, cardToRemove: card.Identity) ?usize {
+        const index = self.lookUpByID(cardToRemove);
+
+        if (index) |valid_index| {
+            _ = self.cards.orderedRemove(valid_index);
+            return valid_index;
         }
-        std.debug.print("[Deck - removeSingleCard] Card not found: {}\n", .{cardToRemove});
+        return null;
+    }
+
+    /// Removes a single card instance from the given index
+    pub fn removeCardByIndex(self: *PlayingCards, index: usize) ?card.Identity {
+        if (index < self.cards.items.len) {
+            return self.cards.orderedRemove(index);
+        }
+        return null;
     }
 
     /// Removes all instances of the given card from the deck.
-    pub fn removeCards(self: *Cards, cardToRemove: card.CAttributes) !void {
-        var found = true;
-        while (found) {
-            found = false;
-            for (self.cards.items, 0..) |currentCard, index| {
-                if (currentCard.rank == cardToRemove.rank and currentCard.suit == cardToRemove.suit) {
-                    _ = self.cards.orderedRemove(index);
-                    found = true;
-                    break;
-                }
+    pub fn removeMultipleCardsByID(self: *PlayingCards, cardToRemove: card.Identity) void {
+        var i: usize = 0;
+        while (i < self.cards.items.len) {
+            if (card.Identity.isCardEqual(self.cards.items[i], cardToRemove)) {
+                _ = self.cards.orderedRemove(i);
+                // if a card is removed, the next card shifts into the next index, so no need to increment.
+            } else {
+                i += 1;
             }
         }
     }
 
     /// Returns the card at the specified index, if valid.
-    pub fn lookUp(self: *Cards, index: usize) !card.CAttributes {
+    pub fn lookUpByIndex(self: *PlayingCards, index: usize) ?card.Identity {
         if (index < self.cards.items.len) {
             return self.cards.items[index];
         } else {
-            std.debug.print("[Deck - lookUp] Index out of range: {} >= {}\n", .{ index, self.cards.items.len });
+            return null;
         }
     }
 
-    /// Returns a random card from the deck.
-    pub fn randomLookUp(self: *Cards) !card.CAttributes {
-        var rng = std.Random.DefaultPrng.init(@intCast(std.time.nanoTimestamp()));
-        return self.cards.items[rng.random().uintLessThan(usize, self.cards.items.len)];
+    /// Returns the index of a given card identifier, returns null if nothing was found
+    pub fn lookUpByID(self: *PlayingCards, cardToSearch: card.Identity) ?usize {
+        for (self.cards.items, 0..) |currentCard, index| {
+            if (card.Identity.isCardEqual(cardToSearch, currentCard)) {
+                return index;
+            }
+        }
+        return null;
     }
 
-    /// Shuffles the deck. Pass `null` for a random seed.
-    pub fn shuffle(self: *Cards, seed: ?u64) void {
-        var rng = if (seed) {
-            std.Random.DefaultPrng.init(seed);
+    pub fn cardExists(self: *PlayingCards, cardToCheck: card.Identity) bool {
+        for (self.cards.items) |currentCard| {
+            if (card.Identity.isCardEqual(cardToCheck, currentCard)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    pub fn countCardType(self: *PlayingCards, cardToCount: card.Identity) usize {
+        var counter: usize = 0;
+        for (self.cards.items) |currentCard| {
+            if (card.Identity.isCardEqual(cardToCount, currentCard)) {
+                counter += 1;
+            }
+        }
+        return counter;
+    }
+
+    /// Returns a random card from the deck without reshuffling.
+    pub fn randomLookUp(self: *PlayingCards, seed: ?u64) ?card.Identity {
+        if (self.cards.items.len == 0) return null;
+
+        var rng = if (seed) |valid_Seed| {
+            std.Random.DefaultPrng.init(valid_Seed);
         } else {
             std.Random.DefaultPrng.init(@intCast(std.time.nanoTimestamp()));
         };
 
-        rng.random().shuffle(card.CAttributes, self.cards.items);
+        return self.cards.items[rng.random().uintLessThan(usize, self.cards.items.len)];
+    }
+
+    // Shuffle deck and pick the first card.
+    pub fn shuffleLookUp(self: *PlayingCards, seed: ?u64) ?card.Identity {
+        if (self.cards.items.len == 0) return null;
+
+        self.shuffle(seed);
+        return self.cards.items[0];
+    }
+
+    /// Shuffles the deck. Pass `null` for a random seed.
+    pub fn shuffle(self: *PlayingCards, seed: ?u64) void {
+        var rng = if (seed) |valid_seed| {
+            std.Random.DefaultPrng.init(valid_seed);
+        } else {
+            std.Random.DefaultPrng.init(@intCast(std.time.nanoTimestamp()));
+        };
+
+        rng.random().shuffle(card.Identity, self.cards.items);
     }
 };
