f3f660ead60fcad61177ebebcdd005b84a464150
ene01 <masqufeik@gmail.com>
2025-11-05 16:16:12 -0300

    Update names and methods, expose 'player.Attributes' directly

--- src/game.zig
+++ src/game.zig
@@ -1,18 +1,16 @@
 const std = @import("std");
 const deck = @import("deck.zig");
-const player = @import("player.zig");
-const rank = @import("rank.zig");
-const suit = @import("suit.zig");
 const card = @import("card.zig");
+pub const player = @import("player.zig").Attributes;
 
 // TODO: Needs the game implementation itself...
 pub const Cardgame = struct {
-    deck: deck.PlayingCards,
-    player: player.Attributes,
+    deck: deck.CardList,
+    player: player,
 
     /// Initiation of Cardgame.
     pub fn init(alloc: std.mem.Allocator, defaultColumnAmount: usize) !Cardgame {
-        return Cardgame{ .deck = deck.PlayingCards.init(alloc), .player = try player.Attributes.init(alloc, defaultColumnAmount) };
+        return Cardgame{ .deck = deck.CardList.init(alloc), .player = try player.init(alloc, defaultColumnAmount) };
     }
 
     pub fn giveCardsToPlayer(self: *Cardgame) !void {
@@ -37,8 +35,8 @@ pub const Cardgame = struct {
     /// Adds a standard 52+52 deck of cards for the game.
     pub fn addStandardDeck(self: *Cardgame) !void {
         for (0..2) |_| { // Repeat 2 times
-            for (1..(@intFromEnum(suit.Group.Diamond)) + 1) |suitInt| {
-                for (2..(@intFromEnum(rank.Hierarchy.Ace)) + 1) |rankInt| {
+            for (1..(@intFromEnum(card.Suit.Diamond)) + 1) |suitInt| {
+                for (2..(@intFromEnum(card.Rank.Ace)) + 1) |rankInt| {
                     try self.deck.addCard(card.Identity{ .rank = @enumFromInt(rankInt), .suit = @enumFromInt(suitInt) });
                 }
             }
