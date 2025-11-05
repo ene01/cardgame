01c16694942756c1f3b75ed40dfb4ca09a34ddd3
ene01 <masqufeik@gmail.com>
2025-11-05 16:14:19 -0300

    Rename 'matrix' to 'tableau', apply changes from card.zig and deck.zig, add small test

--- src/player.zig
+++ src/player.zig
@@ -1,24 +1,24 @@
 const std = @import("std");
 const card = @import("card.zig");
-const cardmatrix = @import("cardmatrix.zig");
+const cardmatrix = @import("card_matrix.zig");
 const deck = @import("deck.zig");
 
 /// Player attributes: hand, rows, and status.
 pub const Attributes = struct {
-    hand: deck.PlayingCards,
-    discard_deck: deck.PlayingCards,
-    matrix: cardmatrix.CardMatrix,
+    hand: deck.CardList,
+    discard_deck: deck.CardList,
+    tableau: cardmatrix.CardMatrix,
     is_playing: bool,
 
     /// Initializes and returns a player's attributes with an empty hand.
     pub fn init(alloc: std.mem.Allocator, columnAmount: usize) !Attributes {
-        return Attributes{ .hand = deck.PlayingCards.init(alloc), .discard_deck = deck.PlayingCards.init(alloc), .matrix = try cardmatrix.CardMatrix.init(alloc, columnAmount), .is_playing = true };
+        return Attributes{ .hand = deck.CardList.init(alloc), .discard_deck = deck.CardList.init(alloc), .tableau = try cardmatrix.CardMatrix.init(alloc, columnAmount), .is_playing = true };
     }
 
     /// Releases memory for dynamic arrays (hand).
     pub fn deinit(self: *Attributes) void {
         self.hand.deinit();
-        self.matrix.deinit();
+        self.tableau.deinit();
     }
 
     /// Adds a card to the player's hand.
@@ -28,7 +28,7 @@ pub const Attributes = struct {
 
     /// Adds a card to the specified row.
     pub fn addToRow(self: *Attributes, newCard: card.Identity, row: usize) !void {
-        try self.matrix.addCard(@intCast(row), newCard);
+        try self.tableau.addCard(@intCast(row), newCard);
     }
 
     /// Removes a single instance of a card from the player's hand.
@@ -43,6 +43,18 @@ pub const Attributes = struct {
 
     /// Removes the last card from the specified row.
     pub fn removeCardFromColumn(self: *Attributes, column: usize) ?card.Identity {
-        return self.matrix.removeCard(column);
+        return self.tableau.removeCard(column);
     }
 };
+
+test "player initiation" {
+    const alloc = std.testing.allocator;
+
+    const player_one = try Attributes.init(alloc, 4);
+    defer player_one.deinit();
+
+    try std.testing.expectEqual(player_one.tableau.matrix.items.len, 4);
+    try std.testing.expectEqual(player_one.discard_deck.len(), 0);
+    try std.testing.expectEqual(player_one.hand.len(), 0);
+    try std.testing.expect(player_one.is_playing);
+}
