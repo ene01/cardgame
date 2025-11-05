e9ddeccce29980ecfac45dd7e7b9a643cb880160
ene01 <masqufeik@gmail.com>
2025-11-05 16:58:14 -0300

    Convert 'cardgame' to a library

---
+++ src/root.zig
@@ -0,0 +1,5 @@
+pub const game = @import("game.zig");
+pub const player = @import("player.zig");
+pub const cardmatrix = @import("card_matrix.zig");
+pub const deck = @import("deck.zig");
+pub const card = @import("card.zig");
