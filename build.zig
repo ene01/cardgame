639b97242c222f585dc66455cd94ebcea83df1d5
ene01 <masqufeik@gmail.com>
2025-11-06 10:23:37 -0300

    Simplify as static lib for now

--- build.zig
+++ build.zig
@@ -4,25 +4,27 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    b.modules.put("cardgame", b.addModule("cardgame", .{
-        .root_source_file = b.path("src/root.zig"),
-    })) catch unreachable;
-
-    const static_lib = b.addStaticLibrary(.{
+    const lib = b.addLibrary(.{
         .name = "cardgame",
-        .root_source_file = b.path("src/root.zig"),
-        .target = target,
-        .optimize = optimize,
+        .linkage = .static,
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/root.zig"),
+            .target = b.resolveTargetQuery(target.query),
+            .optimize = optimize,
+        }),
     });
 
-    const tests = b.addTest(.{
-        .root_source_file = b.path("src/root.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-    const run_tests = b.addRunArtifact(tests);
-    const test_step = b.step("test", "Run all library tests");
-    test_step.dependOn(&run_tests.step);
+    b.installArtifact(lib);
 
-    b.installArtifact(static_lib);
+    const test_step = b.step("test", "Run unit tests");
+
+    const unit_tests = b.addTest(.{
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/root.zig"),
+            .target = b.resolveTargetQuery(target.query),
+        }),
+    });
+
+    const run_unit_tests = b.addRunArtifact(unit_tests);
+    test_step.dependOn(&run_unit_tests.step);
 }
