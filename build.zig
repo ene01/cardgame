ea5683e6809defdd6346147b82d91c152519a92f
ene01 <masqufeik@gmail.com>
2025-11-08 19:30:07 -0300

    Simplify for now

--- build.zig
+++ build.zig
@@ -15,16 +15,4 @@ pub fn build(b: *std.Build) void {
     });
 
     b.installArtifact(lib);
-
-    const test_step = b.step("test", "Run unit tests");
-
-    const unit_tests = b.addTest(.{
-        .root_module = b.createModule(.{
-            .root_source_file = b.path("src/root.zig"),
-            .target = b.resolveTargetQuery(target.query),
-        }),
-    });
-
-    const run_unit_tests = b.addRunArtifact(unit_tests);
-    test_step.dependOn(&run_unit_tests.step);
 }
