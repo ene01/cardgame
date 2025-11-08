9e5a5b8f037b828a381a3f7515f0c294f988cf0e
ene01 <masqufeik@gmail.com>
2025-11-08 19:29:37 -0300

    Use correct Zig naming conventions

--- src/player.zig
+++ src/player.zig
@@ -11,8 +11,8 @@ pub const Attributes = struct {
     is_playing: bool,
 
     /// Initializes and returns a player's attributes with an empty hand.
-    pub fn init(gpa: std.mem.Allocator, columnAmount: usize, handSize: usize, discardAmount: usize, cardsPerColumn: usize) !Attributes {
-        return Attributes{ .hand = try deck.CardList.init(gpa, handSize), .discard_deck = try deck.CardList.init(gpa, discardAmount), .tableau = try cardmatrix.CardMatrix.init(gpa, columnAmount, cardsPerColumn), .is_playing = true };
+    pub fn init(gpa: std.mem.Allocator, hand_size: usize, discard_size: usize, columns: usize, column_deck_size: usize) !Attributes {
+        return Attributes{ .hand = try deck.CardList.init(gpa, hand_size), .discard_deck = try deck.CardList.init(gpa, discard_size), .tableau = try cardmatrix.CardMatrix.init(gpa, columns, column_deck_size), .is_playing = true };
     }
 
     /// Releases memory for dynamic arrays (hand).
@@ -23,35 +23,35 @@ pub const Attributes = struct {
     }
 
     /// Adds a card to the player's hand.
-    pub fn addToHand(self: *Attributes, newCard: card.Identity) !void {
-        try self.hand.addCard(newCard);
+    pub fn addToHand(self: *Attributes, new_card: card.Identity) !void {
+        try self.hand.addCard(new_card);
     }
 
     /// Adds a card to the specified row.
-    pub fn addToRow(self: *Attributes, newCard: card.Identity, row: usize) !void {
-        try self.tableau.addCard(@intCast(row), newCard);
+    pub fn addToRow(self: *Attributes, new_card: card.Identity, row_index: usize) !void {
+        try self.tableau.addCard(@intCast(row_index), new_card);
     }
 
     /// Removes a single instance of a card from the player's hand.
-    pub fn removeCardFromHand(self: *Attributes, cardToRemove: card.Identity) ?usize {
-        return self.hand.removeCardByID(cardToRemove);
+    pub fn removeCardFromHand(self: *Attributes, card_to_remove: card.Identity) ?usize {
+        return self.hand.removeCardByID(card_to_remove);
     }
 
     /// Removes all instances of a card from the player's hand.
-    pub fn removeCardsFromHand(self: *Attributes, cardToRemove: card.Identity) ?usize {
-        return self.hand.removeCardByID(cardToRemove);
+    pub fn removeCardsFromHand(self: *Attributes, card_to_remove: card.Identity) ?usize {
+        return self.hand.removeCardByID(card_to_remove);
     }
 
     /// Removes the last card from the specified row.
-    pub fn removeCardFromColumn(self: *Attributes, column: usize) ?card.Identity {
-        return self.tableau.removeCard(column);
+    pub fn removeCardFromColumn(self: *Attributes, column_index: usize) ?card.Identity {
+        return self.tableau.removeCard(column_index);
     }
 };
 
 test "player initiation" {
     const alloc = std.testing.allocator;
 
-    var player_one = try Attributes.init(alloc, 4, 5, 10, 52);
+    var player_one = try Attributes.init(alloc, 5, 10, 4, 52);
     defer player_one.deinit();
 
     try std.testing.expectEqual(player_one.tableau.matrix.items.len, 4);
