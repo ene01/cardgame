7e246215c2d65fcfe1586821990ad22a630696c9
ene01 <masqufeik@gmail.com>
2025-11-09 12:36:44 -0300

    Remove not very "game logic" looking code.

--- src/game.zig
+++ src/game.zig
@@ -13,33 +13,9 @@ pub const Cardgame = struct {
         return Cardgame{ .deck = deck.CardList.init(gpa), .player = try player.init(gpa, default_columns, 5, 10) };
     }
 
-    pub fn giveCardsToPlayer(self: *Cardgame) !void {
-        for (self.player.hand.cards.items.len..5) |_| {
-            const picked_card = self.deck.randomLookUp();
-
-            if (picked_card) |valid_card| {
-                // try self.player.addToHand(valid_card);
-                _ = self.deck.removeCardByID(valid_card);
-            } else {
-                return;
-            }
-        }
-    }
-
     /// Deinit allocator data.
     pub fn deinit(self: *Cardgame) void {
         self.deck.deinit();
         self.player.deinit();
-    }
-
-    /// Adds a standard 52+52 deck of cards for the game.
-    pub fn addStandardDeck(self: *Cardgame) !void {
-        for (0..2) |_| { // Repeat 2 times
-            for (1..(@intFromEnum(card.Suit.Diamond)) + 1) |suitInt| {
-                for (2..(@intFromEnum(card.Rank.Ace)) + 1) |rankInt| {
-                    try self.deck.addCard(card.Identity{ .rank = @enumFromInt(rankInt), .suit = @enumFromInt(suitInt) });
-                }
-            }
-        }
     }
 };
