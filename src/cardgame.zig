5b633a5a0a554520466d02e51d900a3aac8a54ed
ene01 <masqufeik@gmail.com>
2025-11-13 20:54:37 -0300

    Rename struct, fix deck not having a deck size.

--- src/cardgame.zig
+++ src/cardgame.zig
@@ -3,17 +3,18 @@ const deck = @import("deck.zig");
 const card = @import("card.zig");
 pub const player = @import("player.zig").Attributes;
 
-pub const Cardgame = struct {
+pub const Gameplay = struct {
     deck: deck.CardList,
     player: player,
+    center_deck: deck.CardList,
 
-    /// Initiation of Cardgame.
-    pub fn init(gpa: std.mem.Allocator, default_columns: usize) !Cardgame {
-        return Cardgame{ .deck = deck.CardList.init(gpa), .player = try player.init(gpa, default_columns, 5, 10) };
+    /// Initiation of the game.
+    pub fn init(gpa: std.mem.Allocator, default_columns: usize) !Gameplay {
+        return Gameplay{ .deck = deck.CardList.init(gpa, 108), .player = try player.init(gpa, default_columns, 5, 10), .center_deck = deck.CardList.init(gpa, 15) };
     }
 
     /// Deinit allocator data.
-    pub fn deinit(self: *Cardgame) void {
+    pub fn deinit(self: *Gameplay) void {
         self.deck.deinit();
         self.player.deinit();
     }
