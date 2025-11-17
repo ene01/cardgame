479a07578ea4e2d1c12e91d954dfb1b6ff48f326
ene01 <masqufeik@gmail.com>
2025-11-17 19:25:18 -0300

    Apply changes from new imports.

--- src/root.zig
+++ src/root.zig
@@ -1,9 +1,9 @@
-pub const cardmatrix = @import("card_matrix.zig");
-pub const card_utlils = @import("card_utils.zig");
-pub const card = @import("card.zig");
-pub const cardgame = @import("cardgame.zig");
+pub const Cardmatrix = @import("card_matrix.zig");
+pub const Card = @import("card.zig");
+pub const Cardgame = @import("cardgame.zig");
+pub const Deck = @import("deck.zig");
+pub const Player = @import("player.zig");
+
 pub const deck_utils = @import("deck_utils.zig");
-pub const deck = @import("deck.zig");
-pub const player = @import("player.zig");
+pub const card_utils = @import("card_utils.zig");
 pub const game_logic = @import("game_logic.zig");
-pub const log = @import("log.zig");
