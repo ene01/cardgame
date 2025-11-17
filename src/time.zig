9f51825b405b16e87f2f7ba2da22d1c2cb42fb90
ene01 <masqufeik@gmail.com>
2025-11-17 19:20:23 -0300

    Implement `Zeit` in a separated file for reuse if needed, replace `page_allocator` with `FixedBufferAllocator`.

---
+++ src/time.zig
@@ -0,0 +1,15 @@
+const std = @import("std");
+const zeit = @import("zeit");
+
+pub fn getTime() !zeit.Time {
+    var buffer: [256]u8 = undefined;
+    var fba = std.heap.FixedBufferAllocator.init(buffer[0..]);
+    const alloc = fba.allocator();
+
+    const now_time = try zeit.instant(.{});
+    const local = try zeit.local(alloc, null);
+    const now_local = now_time.in(&local);
+    const dt = now_local.time();
+
+    return dt;
+}
