0dba5b850288a2b7d26bd9792ecce292d1cfa84e
ene01 <masqufeik@gmail.com>
2025-11-17 19:24:55 -0300

    Apply changes from new Imports.

--- src/game_logic.zig
+++ src/game_logic.zig
@@ -1,17 +1,16 @@
 const std = @import("std");
-const card = @import("card.zig");
-const cardgame = @import("cardgame.zig");
-const d_utils = @import("deck_utils.zig");
+const Cardgame = @import("cardgame.zig");
+const deck_utils = @import("deck_utils.zig");
 
 /// Prepares the table and players for a match.
-pub fn prepareTable(g: *cardgame.Gameplay) void {
-    d_utils.appendStandardDeck(g.deck, 2, true);
+pub fn prepareTable(g: *Cardgame) void {
+    deck_utils.appendStandardDeck(g.deck, 2, true);
 
     for (0..4) |_| {
-        g.player.addToHand(d_utils.dealCard(g.deck, null));
+        g.player.addToHand(deck_utils.dealCard(g.deck, null));
     }
 
     for (0..9) |_| {
-        g.player.addToDiscardDeck(d_utils.dealCard(g.deck, null));
+        g.player.addToDiscardDeck(deck_utils.dealCard(g.deck, null));
     }
 }
