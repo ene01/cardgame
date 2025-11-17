2a3e7f89495caaabcd5fa76eb726cfa7e1ce80ea
ene01 <masqufeik@gmail.com>
2025-11-17 20:04:03 -0300

    Separate code into functions.

--- src/logger.zig
+++ src/logger.zig
@@ -1,32 +1,20 @@
 const std = @import("std");
 const time = @import("time.zig");
 
+/// Bytes used for the strings, 1 byte equals 1 character.
+const s_bytes = 1048;
+
 pub const MessageType = enum(u8) {
     Info,
     Warn,
     Error,
 };
 
-/// Writes a message to a generic log file, the message contains the time of logging, type of message, function from where it came from and the message itself.
+/// Writes and creates a message to a named log file, the message contains the time of logging, type of message, function from where it came from and the message itself.
 /// The log message already contains a new line character, so the message itself does not need one.
-pub fn write(src: std.builtin.SourceLocation, lvl: MessageType, comptime fmt: []const u8, args: anytype) !void {
-    const cwd = std.fs.cwd();
-    const fname = "log.txt";
-
-    var file = try cwd.createFile(fname, .{ .truncate = false });
-    defer file.close();
-
-    _ = try file.seekFromEnd(0);
-
+/// If the file already exists, then it will just keep writting bellow the existing text.
+pub fn writeTo(name: []const u8, src: std.builtin.SourceLocation, lvl: MessageType, comptime fmt: []const u8, args: anytype) !void {
     const current_time = try time.getTime();
-
-    const m_type = switch (lvl) {
-        .Info => "INFO",
-        .Warn => "WARN",
-        .Error => "ERROR",
-    };
-
-    const origin = src.fn_name;
 
     const hour = current_time.hour;
     const min = current_time.minute;
@@ -39,22 +27,56 @@ pub fn write(src: std.builtin.SourceLocation, lvl: MessageType, comptime fmt: []
     const corrected_min = if (min < 10) try std.fmt.bufPrint(m_buffer[0..], "0{}", .{min}) else try std.fmt.bufPrint(m_buffer[0..], "{}", .{min});
     const corrected_sec = if (sec < 10) try std.fmt.bufPrint(s_buffer[0..], "0{}", .{sec}) else try std.fmt.bufPrint(s_buffer[0..], "{}", .{sec});
 
-    var fs_buffer: [512]u8 = undefined;
+    const m_type = switch (lvl) {
+        .Info => "INFO",
+        .Warn => "WARN",
+        .Error => "ERROR",
+    };
+
+    const origin = src.fn_name;
+
+    var fs_buffer: [s_bytes]u8 = undefined;
     const formatted_string = try std.fmt.bufPrint(fs_buffer[0..], fmt, args);
 
-    var ms_buffer: [512]u8 = undefined;
+    var ms_buffer: [s_bytes]u8 = undefined;
     const message = try std.fmt.bufPrint(
         ms_buffer[0..],
         "[{s}:{s}:{s}] [{s}] {s} | {s}\n",
         .{ corrected_hour, corrected_min, corrected_sec, m_type, origin, formatted_string },
     );
 
-    try file.writeAll(message);
+    try simpleWrite(name, "{s}", .{message});
+}
+
+/// Writes and creates a message to a generic log file, the message contains the time of logging, type of message, function from where it came from and the message itself.
+/// The log message already contains a new line character, so the message itself does not need one.
+/// If the file already exists, then it will just keep writting bellow the existing text.
+pub fn write(src: std.builtin.SourceLocation, lvl: MessageType, comptime fmt: []const u8, args: anytype) !void {
+    try writeTo("log.txt", src, lvl, fmt, args);
+}
+
+/// Writes a simple message file, the log will only contain the message given.
+/// The log message already contains a new line character, so the message itself does not need one.
+/// If the file already exists, then it will just keep writting bellow the existing text.
+pub fn simpleWrite(name: []const u8, comptime fmt: []const u8, args: anytype) !void {
+    const cwd = std.fs.cwd();
+
+    var file = try cwd.createFile(name, .{ .truncate = false });
+    defer file.close();
+
+    _ = try file.seekFromEnd(0);
+
+    var fs_buffer: [s_bytes]u8 = undefined;
+    const formatted_string = try std.fmt.bufPrint(fs_buffer[0..], fmt, args);
+
+    try file.writeAll(formatted_string);
 }
 
 test "logging" {
     const string = "something has gone wrong";
-    try write(@src(), MessageType.Info, "This is a test, if this doesn't appear, then {s}", .{string});
+    try writeTo("test.txt", @src(), MessageType.Info, "This is a test, if this doesn't appear, then {s}", .{string});
+    try write(@src(), MessageType.Info, "This is also a test, you better be reading this, because if not, then {s}", .{string});
+    try simpleWrite("simple.txt", "Simple test, if not being read, then {s}", .{string});
 
     // this test is very prone to fail when not coded properly or not having the correct params, so im just assuming that not having an error means success.
     try std.testing.expect(true);
