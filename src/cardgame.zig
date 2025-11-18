12cb2105b17d5ffe643025c263598eb1ff8b9be9
ene01 <masqufeik@gmail.com>
2025-11-17 23:34:08 -0300

    Change names of variables.

--- src/cardgame.zig
+++ src/cardgame.zig
@@ -4,18 +4,18 @@ pub const Player = @import("player.zig");
 
 pub const Cardgame = @This();
 
-current_deck: Deck,
-current_player: Player,
-current_center_deck: Deck,
+deck: Deck,
+player: Player,
+center_deck: Deck,
 
 /// Initiation of the game.
 pub fn init(gpa: std.mem.Allocator, default_columns: usize) !Cardgame {
-    return Cardgame{ .current_deck = Deck.init(gpa, 108), .current_player = try Player.init(gpa, default_columns, 5, 10), .current_center_deck = Deck.init(gpa, 15) };
+    return Cardgame{ .deck = Deck.init(gpa, 108), .player = try Player.init(gpa, default_columns, 5, 10), .center_deck = Deck.init(gpa, 15) };
 }
 
 /// Deinit allocator data.
 pub fn deinit(self: *Cardgame) void {
-    self.current_deck.deinit();
-    self.current_center_deck.deinit();
-    self.current_player.deinit();
+    self.deck.deinit();
+    self.center_deck.deinit();
+    self.player.deinit();
 }
