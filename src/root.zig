e4eba7e17ead1573a899eac81962ab9e5339fc72
ene01 <masqufeik@gmail.com>
2025-11-24 16:15:40 -0300

    Renames.

--- src/root.zig
+++
@@ -1,9 +0,0 @@
-pub const Cardmatrix = @import("card_matrix.zig");
-pub const Card = @import("card.zig");
-pub const Cardgame = @import("cardgame.zig");
-pub const Deck = @import("deck.zig");
-pub const Player = @import("player.zig");
-
-pub const deck_utils = @import("deck_utils.zig");
-pub const card_utils = @import("card_utils.zig");
-pub const game_logic = @import("game_logic.zig");
